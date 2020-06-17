7c4228afee2ac3c3bd6b91e344d68d8ba72fbc75
Andrey Popov <andpop@mail.ru>
2020-06-17 07:18:51 +0300

    Begin Even game

---
+++ src/games/Even.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+const TRY_COUNT = 3;
+
+function isEven($number)
+{
+    return ($num % 2) === 0;
+}
+
+function getName()
+{
+    return prompt('May I have your name?');
+}
+
+function showDescription()
+{
+    line('Welcome to the Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line('');
+}
+
+function showGreeting($name)
+{
+    line("Hello, %s!", $name);
+}
+
+function run()
+{
+    showDescription();
+    $name = getName();
+    showGreeting($name);
+
+}
